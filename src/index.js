76d3d32106982f70053adba5bc87858ac3c8515d
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-07-25 18:14:44 +0300

    feat: implement task managment features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import taskRouter from "./modules/task/task.route.js";
 dotenv.config();
 const PORT = process.env.PORT ?? 3000;
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, Express with ES Modules!");
 });
+app.use("/api", taskRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
