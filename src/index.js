2a817238e8ec9a6ad48bc08fe9391570ea92e49b
abdelrahmann22 <abdelrahmannn202@gmail.com>
2025-07-28 23:44:29 +0300

    feat: integrate chat routes and WebSocket server in main application

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createServer } from "node:http";
 import taskRouter from "./modules/task/task.route.js";
 dotenv.config();
 import "./config/database.js";
+import { initializeSocket } from "./socket/socket.config.js";
 import projectRouter from "./modules/project/project.route.js";
 import projectMembersRouter from "./modules/projectMembers/projectMembers.route.js";
 import userRouter from "./modules/user/user.route.js";
 import authRouter from "./modules/auth/auth.route.js";
+import chatRouter from "./modules/chat/chat.route.js";
 import { errorHandler } from "./middleware/errorHandler.middleware.js";
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
 
 app.use(express.json());
 
 // Api Routes
 app.use("/api", authRouter);
+app.use("/api", chatRouter);
 app.use("/api", userRouter);
 app.use("/api", projectRouter);
 app.use("/api", projectMembersRouter);
@@ -37,6 +42,10 @@ app.use((req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+const io = initializeSocket(server);
+
+export { app, io };
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
