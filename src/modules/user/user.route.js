1bf5bdf4780475d0a16b2f3c5d73e76a8584a2c9
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-07-29 20:58:12 +0300

    feat: add user profile route with authentication middleware

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllUsers, getUserByEmail } from "./user.controller.js";
-
+import {
+  getAllUsers,
+  getUserByEmail,
+  getUserProfile,
+} from "./user.controller.js";
+import { verifyLogin } from "../../middleware/auth.middleware.js";
 const userRouter = express.Router();
 
 userRouter.route("/user").get(getAllUsers).post(getUserByEmail);
-
+userRouter.get("/user/profile", verifyLogin, getUserProfile);
 export default userRouter;
